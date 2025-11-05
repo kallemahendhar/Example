613470d5ad8c762598ea3a56cf0433da7c5f3f83
kallemahendhar <kallemahendhar1311@gmail.com>
2025-11-05 13:08:14 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class Add{
+    public static void main(String[] args) {
+        int a=1;
+        int b=10;
+        int sum = a+b;
+        System.out.println("Sum: " + sum);
+    }
+}
