dce4341369e2b4e9cb00cb12a0f3da5e68fc92df
kallemahendhar <kallemahendhar1311@gmail.com>
2025-11-12 22:47:09 +0530

    Updated Add code

--- Add.java
+++ Add.java
@@ -1,10 +1,12 @@
-
 import java.util.*;
 class Add{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.print("Sum: "+(a+b));
+        System.out.println("Sum: "+(a+b));
+        System.out.println("Mul "+(a*b));
+        System.out.println("Sub "+(a-b));
+        System.out.println("Div "+(a/b));
     }
 }
